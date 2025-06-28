01143cf88c197a588e9acc49a835d48b90ff2230
Artemy Medvedev <artemiy.logmy@gmail.com>
2025-06-28 18:38:47 +0500

    hyperparams tuning, back to normal network conplexity, saving params

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import torch
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 
 # loading the dataset
 imagenet = load_dataset(
-    'Maysee/tiny-imagenet', 
-    split='valid',
+  'Maysee/tiny-imagenet', 
+  split='valid',
 )
 
 # preprocessing the dataset (converting to RGB and to tensors)
@@ -16,14 +15,12 @@ images = []
 labels = []
 
 for img in imagenet:
-    rbgImg = img['image'].convert('RGB')
-    resizedImg = rbgImg.resize((227, 227))
-    pixels = np.array(resizedImg).astype(np.float32) / 255.0
-    permuted = np.transpose(pixels, (2, 0, 1))
-    images.append(permuted)
-    labels.append(img['label'])
-
-print(len(images))
+  rbgImg = img['image'].convert('RGB')
+  resizedImg = rbgImg.resize((227, 227))
+  pixels = np.array(resizedImg).astype(np.float32) / 255.0
+  permuted = np.transpose(pixels, (2, 0, 1))
+  images.append(permuted)
+  labels.append(img['label'])
 
 # creating the nn API
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
@@ -31,7 +28,7 @@ g = torch.Generator(device=device).manual_seed(42)
 
 class Conv:
   def __init__(self, num_of_kernels, channels_in, kernel_size, stride, padding):
-    self.weight = torch.randn((num_of_kernels, channels_in, kernel_size, kernel_size), generator=g, device=device) * 0.01
+    self.weight = torch.randn((num_of_kernels, channels_in, kernel_size, kernel_size), generator=g, device=device) * 0.1
     self.stride = stride
     self.padding = padding
   
@@ -55,11 +52,8 @@ class Pooling:
     return []
   
 class Flatten:
-  def __init__(self, batch_size):
-    self.batch_size = batch_size
-  
   def __call__(self, x):
-    self.out = x.reshape(self.batch_size, -1)
+    self.out = x.reshape(x.shape[0], -1)
     return self.out
   
   def parameters(self):
@@ -88,16 +82,16 @@ class ReLU:
   
 # initializing the NN
 batch_size = 16
-learning_rate = 0.01
-num_of_epochs = 100_000
+learning_rate = 0.0001
+num_of_epochs = 10
 
 layers = [
   Conv(num_of_kernels=96, channels_in=3, kernel_size=11, stride=4, padding=0), ReLU(), Pooling(kernel_size=3, stride=2),
   Conv(num_of_kernels=256, channels_in=96, kernel_size=5, stride=1, padding=2), ReLU(), Pooling(kernel_size=3, stride=2),
-  # Conv(num_of_kernels=384, channels_in=256, kernel_size=3, stride=1, padding=1), ReLU(),
-  # Conv(num_of_kernels=384, channels_in=384, kernel_size=3, stride=1, padding=1), ReLU(),
-  Conv(num_of_kernels=256, channels_in=256, kernel_size=3, stride=1, padding=1), ReLU(), Pooling(kernel_size=3, stride=2),
-  Flatten(batch_size),
+  Conv(num_of_kernels=384, channels_in=256, kernel_size=3, stride=1, padding=1), ReLU(),
+  Conv(num_of_kernels=384, channels_in=384, kernel_size=3, stride=1, padding=1), ReLU(),
+  Conv(num_of_kernels=256, channels_in=384, kernel_size=3, stride=1, padding=1), ReLU(), Pooling(kernel_size=3, stride=2),
+  Flatten(),
   Linear(fan_in=9216, fan_out=4096), ReLU(),
   Linear(fan_in=4096, fan_out=4096), ReLU(),
   Linear(fan_in=4096, fan_out=1000), ReLU(),
@@ -107,57 +101,58 @@ layers = [
 # requiring gradient on all layer's parameters
 params = []
 for layer in layers:
-    params += layer.parameters()
+  params += layer.parameters()
 
 for p in params:
-    p.requires_grad = True
-
-for p in params:
-    p.requires_grad = True
+  p.requires_grad = True
 
 def activation_plot(tensor):
-    # black = false; white = true
-    h_detached = tensor.cpu().detach().view(132, -1)
-    plt.imshow(h_detached <= 0, cmap="gray", vmin=0, vmax=1) 
-    plt.show()
+  # black = false; white = true
+  h_detached = tensor.cpu().detach().view(132, -1)
+  plt.imshow(h_detached <= 0, cmap="gray", vmin=0, vmax=1) 
+  plt.show()
 
 # training loop
 for epoch in range(num_of_epochs):
-    # sampling a random batch
-    randomIndecies = [random.randint(0, 10000-1) for _ in range(batch_size)]
-    image_batch = []
-    ans = []
-    for i in randomIndecies:
-        image_batch.append(images[i])
-        ans.append(labels[i])
+  # sampling a random batch
+  randomIndecies = [random.randint(0, 10000-1) for _ in range(batch_size)]
+  image_batch = []
+  ans = []
+  for i in randomIndecies:
+    image_batch.append(images[i])
+    ans.append(labels[i])
 
-    image_batch = torch.tensor(image_batch).to(torch.float32).to(device)
-    ans = torch.tensor(ans).to(device)
+  image_batch = torch.tensor(image_batch).to(torch.float32).to(device)
+  ans = torch.tensor(ans).to(device)
 
-    # forward pass
-    def forward(image_batch):
-        for layer in layers:
-            image_batch = layer(image_batch)
-            # activation_plot(params[0])
-            # print(layer, image_batch.shape)
-        return image_batch
+  # forward pass
+  def forward(image_batch):
+    for layer in layers:
+      image_batch = layer(image_batch)
 
-    # backward pass
-    def backward(logits, ans):
-        global learning_rate
-        loss = torch.nn.functional.cross_entropy(logits, ans)
-        print(f"epoch {epoch}, loss = {loss}")
+    return image_batch
 
-        for p in params:
-            p.grad = None
+  # backward pass
+  def backward(logits, ans):
+    global learning_rate
+    loss = torch.nn.functional.cross_entropy(logits, ans)
+    print(f"epoch {epoch}, loss = {loss}")
 
-        loss.backward()
+    for p in params:
+      p.grad = None
 
-        if epoch > 50_000:
-           learning_rate = 0.005
+    loss.backward()
 
-        for p in params:
-            p.data -= p.grad * learning_rate
-    
-    logits = forward(image_batch)
-    backward(logits, ans)
+    for p in params:
+      p.data -= p.grad * learning_rate
+  
+  logits = forward(image_batch)
+  backward(logits, ans)
+
+def save_params():
+  for layer in layers:
+    layer_params = layer.parameters()
+    if layer_params:
+      torch.save(layer_params, f"params/{layer}.pth")
+
+save_params()
